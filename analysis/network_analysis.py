577d81077a4fd90c27877d2d1b7d75e9d1f55e04
Mateusz Knapik <m.knapik6@gmail.com>
2020-05-05 23:14:04 +0200

    kNN calculation updated

--- analysis/network_analysis.py
+++ analysis/network_analysis.py
@@ -5,6 +5,7 @@ from keras import backend as K
 from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import TSNE
 import seaborn as sns
+from sklearn.neighbors import NearestNeighbors
 from network.constants import TSNE_PATH_PREFIX
 
 
@@ -43,14 +44,32 @@ def show_tsne(model_name, epochs, X, Y):
     data = StandardScaler().fit_transform(X)
     targets = np.argmax(Y, axis=1)
 
-    file_path = TSNE_PATH_PREFIX + model_name + "_" + str(epochs)
+    file_path = TSNE_PATH_PREFIX + model_name + "_" + str(epochs) + ".npy"
     if exists(file_path):
         points_transformed = np.load(file_path)
     else:
-        points_transformed = TSNE(n_components=2, metric="euclidean", perplexity=60).fit_transform(data).T
+        points_transformed = TSNE(n_components=2, perplexity=30).fit_transform(data).T
         np.save(file_path, points_transformed)
 
     palette = sns.color_palette("bright", 10)
     plt.figure(figsize=(10, 10))
     sns.scatterplot(points_transformed[0, :], points_transformed[1, :], hue=targets, legend='full', palette=palette)
     plt.show()
+
+    return points_transformed.reshape(points_transformed.shape[1], 2)
+
+
+def get_knn_accuracy(X, Y):
+    knn = NearestNeighbors(n_neighbors=7)
+    knn.fit(X)
+
+    ratio = 0
+    Y = np.argmax(Y, axis=1)
+    for neighbors in knn.kneighbors(X, return_distance=False):
+        count = 0
+        for neighbor in neighbors[1:]:
+            if Y[neighbors[0]] == Y[neighbor]:
+                count += 1
+        ratio += count / 6
+
+    return ratio / X.shape[0]
--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from analysis.network_analysis import *
 
 
 def main():
-    X_train, Y_train, x_test, y_test = load_data_mlp(DataType.SVHN)
-    model = create_multilayer_perceptron(DataType.SVHN)
-    train_model(model, 16, 3, "svhn_mlp", X_train, Y_train)
+    X_train, Y_train, X_test, Y_test = load_data_mlp(DataType.MNIST)
+    # model = create_multilayer_perceptron(DataType.MNIST)
+    # train_model(model, 16, 3, "mnist_mlp", X_train, Y_train)
     # model_cnn = create_cnn(DataType.CIFAR)
     # train_model(model_cnn, 32, 1, "cifar_cnn", X_train, Y_train)
-    network_history = load_network_history_from_file("svhn_mlp", 3)
-    plot_history(network_history)
-    # model = load_model_from_file("mnist_cnn", 10)
-    # l1, l2 = get_activations_cnn(model, X_train)
-    # show_tsne("mnist_cnn_l1", 10, l1, Y_train[:2000])
+    # network_history = load_network_history_from_file("svhn_mlp", 3)
+    # plot_history(network_history)
+    # model = load_model_from_file("mnist_mlp", 3)
+    # l1, l2, l3, l4 = get_activations_mlp(model, X_test)
+    transformed_points = show_tsne("mnist_raw_test", 10000, X_test, Y_test)
+    print(get_knn_accuracy(transformed_points, Y_test))
 
 
 if __name__ == '__main__':
